46ff79bd92aef3a0e25745ab5d2d91a0e7a697b1
simpulton <simpul@gmail.com>
2013-09-16 13:59:47 -0700

    Now with ngTouch and dynamic animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('website', ['ngAnimate'])
+angular.module('website', ['ngAnimate', 'ngTouch'])
     .controller('MainCtrl', function ($scope) {
         $scope.slides = [
             {image: 'images/img00.jpg', description: 'Image 00'},
@@ -8,9 +8,11 @@ angular.module('website', ['ngAnimate'])
             {image: 'images/img04.jpg', description: 'Image 04'}
         ];
 
+        $scope.direction = 'left';
         $scope.currentIndex = 0;
 
         $scope.setCurrentSlideIndex = function (index) {
+            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
             $scope.currentIndex = index;
         };
 
@@ -19,28 +21,43 @@ angular.module('website', ['ngAnimate'])
         };
 
         $scope.prevSlide = function () {
+            $scope.direction = 'left';
             $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
         };
 
         $scope.nextSlide = function () {
+            $scope.direction = 'right';
             $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
         };
     })
     .animation('.slide-animation', function () {
         return {
             addClass: function (element, className, done) {
+                var scope = element.scope();
+
                 if (className == 'ng-hide') {
-                    TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
                 }
                 else {
                     done();
                 }
             },
             removeClass: function (element, className, done) {
+                var scope = element.scope();
+
                 if (className == 'ng-hide') {
                     element.removeClass('ng-hide');
 
-                    TweenMax.set(element, { left: element.parent().width() });
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.set(element, { left: startPoint });
                     TweenMax.to(element, 0.5, {left: 0, onComplete: done });
                 }
                 else {
